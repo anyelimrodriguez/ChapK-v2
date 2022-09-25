98f2b3bcf730bf588d30c1d7acddfd3450eb811b
PreetomCho <preetom56@gmail.com>
2022-09-24 23:45:05 -0400

    fixed main.dart back to welcome page

--- chap_k/lib/main.dart
+++ chap_k/lib/main.dart
@@ -18,7 +18,7 @@ class Chap_K extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/Write',
+      initialRoute: '/WelcomePage',
       routes: {
         '/':(context) => WelcomePage(),
         '/SignUp':(context) => SignUp(),
--- chap_k/lib/pages/Write.dart
+++ chap_k/lib/pages/Write.dart
@@ -85,4 +85,3 @@ class PostButton extends StatelessWidget{
   }
 
 }
-
