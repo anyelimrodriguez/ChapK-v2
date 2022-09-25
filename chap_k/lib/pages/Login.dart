98d26a1f073e932475767618b370f6b9a6972c68
Kevin635 <experthead07@gmail.com>
2022-09-25 11:06:22 -0400

    Got rid of the scroll view

--- chap_k/lib/pages/Login.dart
+++ chap_k/lib/pages/Login.dart
@@ -16,127 +16,128 @@ class _LoginState extends State<Login> {
       backgroundColor: Color(0xFFC3B1E1),
       body: Center(
         child: SafeArea(
-          child: SingleChildScrollView(
-            child: Container(
-              alignment: Alignment.center,
-              // padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
-              margin: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(width: 1, color: Colors.black)),
-              height: MediaQuery.of(context).size.height,
-              width: 500,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          SizedBox(
-                            height: 40,
+          // child: SingleChildScrollView(
+
+          child: Container(
+            alignment: Alignment.center,
+            // padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+            margin: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(width: 1, color: Colors.black)),
+            height: MediaQuery.of(context).size.height,
+            width: 500,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Text(
+                          "Login",
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Text(
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        // Text(
+                        //   "Create an Account,Its free",
+                        //   style: TextStyle(
+                        //     fontSize: 15,
+                        //     color: Colors.grey[700],
+                        //   ),
+                        // ),
+                        SizedBox(
+                          height: 30,
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Column(
+                        children: [
+                          makeInput(label: "Email"),
+                          makeInput(label: "Password", obsureText: false),
+                          // makeInput(label: "Confirm Pasword", obsureText: true)
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Container(
+                        padding: EdgeInsets.only(top: 3, left: 3),
+                        child: MaterialButton(
+                          hoverColor: Colors.amber,
+                          minWidth: double.infinity,
+                          height: 60,
+                          onPressed: () {},
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(40)),
+                          child: Text(
                             "Login",
                             style: TextStyle(
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          // Text(
-                          //   "Create an Account,Its free",
-                          //   style: TextStyle(
-                          //     fontSize: 15,
-                          //     color: Colors.grey[700],
-                          //   ),
-                          // ),
-                          SizedBox(
-                            height: 30,
-                          )
-                        ],
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 40),
-                        child: Column(
-                          children: [
-                            makeInput(label: "Email"),
-                            makeInput(label: "Password", obsureText: false),
-                            // makeInput(label: "Confirm Pasword", obsureText: true)
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 40),
-                        child: Container(
-                          padding: EdgeInsets.only(top: 3, left: 3),
-                          child: MaterialButton(
-                            hoverColor: Colors.amber,
-                            minWidth: double.infinity,
-                            height: 60,
-                            onPressed: () {},
-                            color: Colors.white,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(40)),
-                            child: Text(
-                              "Login",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 16,
-                                  color: Color(0xFFC3B1E1)),
-                            ),
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16,
+                                color: Color(0xFFC3B1E1)),
                           ),
                         ),
                       ),
-                      SizedBox(
-                        height: 20,
-                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
 
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Don't have an account? "),
-                          // Text(
-                          //   "Sign Up",
-                          //   style: TextStyle(
-                          //       fontWeight: FontWeight.w600, fontSize: 18),
-                          // ),
-                        ],
-                      ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Don't have an account? "),
+                        // Text(
+                        //   "Sign Up",
+                        //   style: TextStyle(
+                        //       fontWeight: FontWeight.w600, fontSize: 18),
+                        // ),
+                      ],
+                    ),
 
-                      //------------
+                    //------------
 
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          SignupButton(),
-                        ],
-                      ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        SignupButton(),
+                      ],
+                    ),
 
-                      //-------------
+                    //-------------
 
-                      //Added Logo
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          // SizedBox(height: 10.0, width: 10.0),
-                          Image.asset(
-                            '../../imgs/logo.png',
-                            fit: BoxFit.contain,
-                            height: 200,
-                            width: 200,
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                    //Added Logo
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        // SizedBox(height: 10.0, width: 10.0),
+                        Image.asset(
+                          '../../imgs/logo.png',
+                          fit: BoxFit.contain,
+                          height: 200,
+                          width: 200,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
+          // ),
         ),
       ),
     );
