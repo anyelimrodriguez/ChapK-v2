5da5478c48447dc7119ccf7d945b2ab52960f646
Calvin Wang <calvinwang1002@gmail.com>
2022-09-25 12:44:49 -0400

    Made reusable Post widget, abstracted buttons, made code more readable and efficient

--- chap_k/lib/pages/Home.dart
+++ chap_k/lib/pages/Home.dart
@@ -8,6 +8,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<Post> posts = [
+    Post(pfp: '../../imgs/DummyImgs/pfp.png', story: 'HI'),
+    Post(pfp: '../../imgs/DummyImgs/pfp2.png', story: 'HOLA'),
+    Post(pfp: '../../imgs/DummyImgs/pfp3.png', story: 'CIAO'),
+    Post(pfp: '../../imgs/DummyImgs/pfp4.png', story: 'BONJOUR'),
+    Post(
+        pfp: '../../imgs/DummyImgs/pfp5.png',
+        story: 'The quick brown fox jumps over the lazy dog')
+  ];
+
   @override
   Widget build(BuildContext context) {
     double wH = MediaQuery.of(context).size.height;
@@ -29,22 +39,7 @@ class _HomeState extends State<Home> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.public, color: Colors.red),
-                    label: Text(
-                      'Language',
-                      style: TextStyle(
-                          color: Colors.blue, fontSize: 30, letterSpacing: 2),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.white,
-                      minimumSize: Size(50, 20),
-                      shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(20.0),
-                      ),
-                    ),
-                  ),
+                  const HomeLanguageButton(),
                   Container(
                       constraints: BoxConstraints(
                         minHeight: 100, //minimum height
@@ -53,193 +48,29 @@ class _HomeState extends State<Home> {
                         maxWidth: wW,
                       ),
                       child: Image(
-                        image: AssetImage('../../imgs/HomeLogo.png'),
+                        image: const AssetImage('../../imgs/HomeLogo.png'),
                         height: wH / 10,
                         width: wW / 10,
                       )),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/');
-                    },
-                    icon: Icon(Icons.logout, color: Color(0xffad5a54)),
-                    label: Text(
-                      'Sign Out',
-                      style: TextStyle(
-                          color: Colors.blue, fontSize: 30, letterSpacing: 2),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.grey.shade200,
-                      minimumSize: Size(50, 20),
-                      shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(20.0),
-                      ),
-                    ),
-                  ),
+                  const HomeSignoutButton(),
                 ],
               ),
             ),
             Padding(
                 padding: EdgeInsets.symmetric(horizontal: wW / 6, vertical: 0),
                 child: Ink(
-                  color: Colors.white,
-                  height: wH - rm,
-                  child: ListView(
-                    shrinkWrap: true,
-                    padding: const EdgeInsets.all(20.0),
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.all(20),
-                            child: CircleAvatar(
-                              radius: 35.0,
-                              backgroundImage:
-                                  AssetImage('../../imgs/DummyImgs/pfp.png'),
-                            ),
-                          ),
-                          Expanded(
-                              child: Container(
-                            child: Container(
-                                padding: EdgeInsets.all(10),
-                                child: Text(
-                                  'HI',
-                                  style: TextStyle(
-                                      fontSize: 12, color: Colors.purple),
-                                )),
-                            // color: Color(0xff9d0505),
-                            margin: EdgeInsets.only(right: 20),
-                            height: 70,
-                            decoration: BoxDecoration(
-                                color: Colors.grey.shade300,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                          )),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.all(20),
-                            child: CircleAvatar(
-                              radius: 35.0,
-                              backgroundImage:
-                                  AssetImage('../../imgs/DummyImgs/pfp2.png'),
-                            ),
-                          ),
-                          Expanded(
-                              child: Container(
-                            child: Container(
-                                padding: EdgeInsets.all(10),
-                                child: Text(
-                                  'HOLA',
-                                  style: TextStyle(
-                                      fontSize: 12, color: Colors.purple),
-                                )),
-                            // color: Color(0xff9d0505),
-                            margin: EdgeInsets.only(right: 20),
-                            height: 70,
-                            decoration: BoxDecoration(
-                                color: Colors.grey.shade300,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                          )),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.all(20),
-                            child: CircleAvatar(
-                              radius: 35.0,
-                              backgroundImage:
-                                  AssetImage('../../imgs/DummyImgs/pfp3.png'),
-                            ),
-                          ),
-                          Expanded(
-                              child: Container(
-                            child: Container(
-                                padding: EdgeInsets.all(10),
-                                child: Text(
-                                  'CIAO',
-                                  style: TextStyle(
-                                      fontSize: 12, color: Colors.purple),
-                                )),
-                            // color: Color(0xff9d0505),
-                            margin: EdgeInsets.only(right: 20),
-                            height: 70,
-                            decoration: BoxDecoration(
-                                color: Colors.grey.shade300,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                          )),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.all(20),
-                            child: CircleAvatar(
-                              radius: 35.0,
-                              backgroundImage:
-                                  AssetImage('../../imgs/DummyImgs/pfp4.png'),
-                            ),
-                          ),
-                          Expanded(
-                              child: Container(
-                            child: Container(
-                                padding: EdgeInsets.all(10),
-                                child: Text(
-                                  'BONJOUR',
-                                  style: TextStyle(
-                                      fontSize: 12, color: Colors.purple),
-                                )),
-                            // color: Color(0xff9d0505),
-                            margin: EdgeInsets.only(right: 20),
-                            height: 70,
-                            decoration: BoxDecoration(
-                                color: Colors.grey.shade300,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                          )),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.all(20),
-                            child: CircleAvatar(
-                              radius: 35.0,
-                              backgroundImage:
-                                  AssetImage('../../imgs/DummyImgs/pfp5.png'),
-                            ),
-                          ),
-                          Expanded(
-                              child: Container(
-                            child: Container(
-                                padding: EdgeInsets.all(10),
-                                child: Text(
-                                  'The quick brown fox jumps over the lazy dog',
-                                  style: TextStyle(
-                                      fontSize: 12, color: Colors.purple),
-                                )),
-                            // color: Color(0xff9d0505),
-                            margin: EdgeInsets.only(right: 20),
-                            height: 70,
-                            decoration: BoxDecoration(
-                                color: Colors.grey.shade300,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                          )),
-                        ],
-                      ),
-                    ],
-                  ),
-                ))
+                    color: Colors.white,
+                    height: wH - rm,
+                    child: ListView(
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.all(20.0),
+                      children: posts
+                          .map((po) => PostWidget(
+                                usrPfp: po.pfp,
+                                usrStory: po.story,
+                              ))
+                          .toList(),
+                    )))
           ],
         ),
         floatingActionButton: Container(
@@ -255,16 +86,108 @@ class _HomeState extends State<Home> {
             onPressed: () {
               Navigator.pushNamed(context, '/Write');
             },
-            child: Text(
+            backgroundColor: Colors.purple,
+            splashColor: Colors.red,
+            hoverColor: const Color(0xff35139d),
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(15.0))),
+            child: const Text(
               'Write',
               style: TextStyle(fontSize: 25, letterSpacing: 2),
             ),
-            backgroundColor: Colors.purple,
-            splashColor: Colors.red,
-            hoverColor: Color(0xff35139d),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(15.0))),
           ),
         ));
+  }
+}
+
+class Post {
+  String pfp;
+  String story;
+
+  Post({required this.pfp, required this.story});
+}
+
+class PostWidget extends StatelessWidget {
+  final String usrPfp;
+  final String usrStory;
+  const PostWidget({super.key, required this.usrPfp, required this.usrStory});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(20),
+          child: CircleAvatar(
+            radius: 35.0,
+            backgroundImage: AssetImage(usrPfp),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            // color: Color(0xff9d0505),
+            margin: const EdgeInsets.only(right: 20),
+            height: 70,
+            decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: const BorderRadius.all(Radius.circular(10))),
+
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                usrStory,
+                style: const TextStyle(fontSize: 12, color: Colors.purple),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class HomeLanguageButton extends StatelessWidget {
+  const HomeLanguageButton({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      onPressed: () {},
+      icon: const Icon(Icons.public, color: Colors.red),
+      label: const Text(
+        'Language',
+        style: TextStyle(color: Colors.blue, fontSize: 30, letterSpacing: 2),
+      ),
+      style: ElevatedButton.styleFrom(
+        primary: Colors.white,
+        minimumSize: const Size(50, 20),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+      ),
+    );
+  }
+}
+
+class HomeSignoutButton extends StatelessWidget {
+  const HomeSignoutButton({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      onPressed: () {
+        Navigator.pushNamed(context, '/');
+      },
+      icon: const Icon(Icons.logout, color: Color(0xffad5a54)),
+      label: const Text(
+        'Sign Out',
+        style: TextStyle(color: Colors.blue, fontSize: 30, letterSpacing: 2),
+      ),
+      style: ElevatedButton.styleFrom(
+        primary: Colors.grey.shade200,
+        minimumSize: const Size(50, 20),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+      ),
+    );
   }
 }
